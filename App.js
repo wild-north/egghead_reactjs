bf647202d1ac8acb1fa217d5d2bac4c3afceb17c
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-12 11:29:15 +0300

    EHREACT-07 react-using-refs-to-access-components

--- App.js
+++ App.js
@@ -4,19 +4,36 @@ import ReactDom from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { text: '' };
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    };
     this.update = this.update.bind(this);
   }
   update(event) {
-    this.setState({ text: event.target.value});
+    this.setState({
+      red:      ReactDom.findDOMNode(this.refs.red).value,
+      green:  ReactDom.findDOMNode(this.refs.green).value,
+      blue:     ReactDom.findDOMNode(this.refs.blue).value
+    });
   }
 
   render() {
     return (
         <div>
-          <Widget text={this.state.text} update={this.update} />
-          <Widget text={this.state.text} update={this.update} />
-          <Widget text={this.state.text} update={this.update} />
+          <div>
+              <Slider ref="red" update={this.update} />
+              R: {this.state.red}&nbsp;
+          </div>
+          <div>
+              <Slider ref="green" update={this.update} />
+              G: {this.state.green}&nbsp;
+          </div>
+          <div>
+              <Slider ref="blue" update={this.update} />
+              B: {this.state.blue}&nbsp;
+          </div>
         </div>
     )
   }
@@ -30,6 +47,15 @@ App.propTypes = {
 
 App.defaultProps = {
   text: "my default text value"
+}
+
+class Slider extends React.Component {
+  render() {
+    return <input type = "range"
+          min="0"
+          max="255"
+          onChange = {this.props.update} />
+  }
 }
 
 const Widget = (props) => {
