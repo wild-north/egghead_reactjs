055355bfb5108acff7a92c2e0cbd09f2a5c36633
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-12 11:50:19 +0300

    EHREACT-08 react-accessing-child-properties

--- App.js
+++ App.js
@@ -1,76 +1,17 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    };
-    this.update = this.update.bind(this);
-  }
-  update(event) {
-    this.setState({
-      red:      ReactDom.findDOMNode(this.refs.red).value,
-      green:  ReactDom.findDOMNode(this.refs.green).value,
-      blue:     ReactDom.findDOMNode(this.refs.blue).value
-    });
-  }
-
   render() {
-    return (
-        <div>
-          <div>
-              <Slider ref="red" update={this.update} />
-              R: {this.state.red}&nbsp;
-          </div>
-          <div>
-              <Slider ref="green" update={this.update} />
-              G: {this.state.green}&nbsp;
-          </div>
-          <div>
-              <Slider ref="blue" update={this.update} />
-              B: {this.state.blue}&nbsp;
-          </div>
-        </div>
-    )
+    return (<Button>React</Button>)
   }
 }
 
-
-App.propTypes = {
-  text: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-  text: "my default text value"
-}
-
-class Slider extends React.Component {
+class Button extends React.Component {
   render() {
-    return <input type = "range"
-          min="0"
-          max="255"
-          onChange = {this.props.update} />
+    return <button>{this.props.children}</button>
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text"
-            onChange={props.update} />
-      <h2>TEXT: {props.text}</h2>
-    </div>
-  );
-}
-
-ReactDom.render(
-  <App text="123123" cat={5}/>,
-  document.getElementById('app')
-);
+const Heart = () => <span className="gliphicon gliphicon-heart"></span>
 
 export default App;
