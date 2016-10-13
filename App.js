5112bdbe5b4b2f0e99b7506ff2e8f8331506e1bc
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-13 18:03:10 +0300

    EHREACT-15 build-a-jsx-live-compiler (NOT FINISHED)

--- App.js
+++ App.js
@@ -1,155 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [
-        {
-          "age": 20,
-          "name": "Hardy Caldwell",
-          "id": 0
-        },
-        {
-          "age": 34,
-          "name": "Riddle Pratt",
-          "id": 1
-        },
-        {
-          "age": 40,
-          "name": "Ellen Young",
-          "id": 2
-        },
-        {
-          "age": 28,
-          "name": "Raymond Valdez",
-          "id": 3
-        },
-        {
-          "age": 23,
-          "name": "Hyde Lynch",
-          "id": 4
-        },
-        {
-          "age": 33,
-          "name": "Lidia Cohen",
-          "id": 5
-        },
-        {
-          "age": 27,
-          "name": "Mckinney Guerra",
-          "id": 6
-        },
-        {
-          "age": 18,
-          "name": "Sybil Graves",
-          "id": 7
-        },
-        {
-          "age": 38,
-          "name": "Phelps Mckay",
-          "id": 8
-        },
-        {
-          "age": 33,
-          "name": "Socorro Patel",
-          "id": 9
-        },
-        {
-          "age": 37,
-          "name": "Rodriguez Weaver",
-          "id": 10
-        },
-        {
-          "age": 34,
-          "name": "Nieves Juarez",
-          "id": 11
-        },
-        {
-          "age": 26,
-          "name": "Cohen Mercado",
-          "id": 12
-        },
-        {
-          "age": 41,
-          "name": "Osborn Vasquez",
-          "id": 13
-        },
-        {
-          "age": 38,
-          "name": "Justine Joyner",
-          "id": 14
-        },
-        {
-          "age": 25,
-          "name": "Snyder Cox",
-          "id": 15
-        },
-        {
-          "age": 24,
-          "name": "Jaime Hall",
-          "id": 16
-        },
-        {
-          "age": 37,
-          "name": "Tracey Hubbard",
-          "id": 17
-        },
-        {
-          "age": 37,
-          "name": "Vasquez Mcpherson",
-          "id": 18
-        },
-        {
-          "age": 28,
-          "name": "Neva Houston",
-          "id": 19
-        },
-        {
-          "age": 18,
-          "name": "Castaneda Castaneda",
-          "id": 20
-        },
-        {
-          "age": 32,
-          "name": "Rosalinda Carter",
-          "id": 21
-        },
-        {
-          "age": 26,
-          "name": "Susanne Sweet",
-          "id": 22
-        },
-        {
-          "age": 39,
-          "name": "Andrea Bernard",
-          "id": 23
-        },
-        {
-          "age": 21,
-          "name": "Ware Martin",
-          "id": 24
-        }
-      ]
+      input: '/* add your JSX here */',
+      output: '',
+      err: ''
+    }
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: babel.transform(code, {
+          stage: 0,
+          loose: 'all'
+        }).code
+      })
+    } catch (err) {
+      this.setState({err: err.message})
     }
   }
   render() {
-    let rows = this.state.data.map(person => {
-      return <PersonRow key={person.id} data={person} />
-    });
-
-    return <table>
-      <tbody>{rows}</tbody>
-    </table>
+    return (
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea name="" id="" cols="30" rows="10"
+                    onChange={this.update}
+                    defaultValue={this.state.input}
+          ></textarea>
+          <pre></pre>
+        </div>
+      </div>
+    );
   }
-}
-
-const PersonRow = (props) => {
-  return <tr>
-    <td>{props.data.id}</td>
-    <td>{props.data.name}</td>
-  </tr>
 }
 
 export default App;
