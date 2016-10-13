45a6b542e1506264b3ca829834126c5be5ce6fce
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-13 11:42:22 +0300

    EHREACT-09 react-component-lifecycle-mounting-basics

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
@@ -7,11 +8,54 @@ class App extends React.Component {
 }
 
 class Button extends React.Component {
+  constructor() {
+    super();
+    this.state = {val: 0};
+    this.update = this.update.bind(this);
+  }
+  update() {
+    this.setState({
+      val: this.state.val + 1
+    })
+  }
+  componentWillMount() {
+      console.log('mounting');
+  }
   render() {
-    return <button>{this.props.children}</button>
+      console.log(`rendering ${this.state.val}`)
+    return <button onClick={this.update}>{this.props.children} {this.state.val}</button>
+  }
+  componentDidMount() {
+    console.log('mounted');
+  }
+  componentWillUnmount() {
+      console.log('bye');
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+  mount() {
+    ReactDOM.render(<App/>, document.getElementById('wrapper'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('wrapper'));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id="wrapper"></div>
+      </div>
+    )
   }
 }
 
 const Heart = () => <span>♥</span>
 
-export default App;
+export default Wrapper;
