1c6d8e988274cb4ab1fd301775b91ed493cc9195
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-13 16:36:11 +0300

    EHREACT-13 react-composable-components

--- App.js
+++ App.js
@@ -1,45 +1,67 @@
 import React from 'react';
-
-let Mixin = (InnerComponent) => class extends React.Component {
-  constructor() {
-    super();
-    this.update = this.update.bind(this);
-    this.state = {val: 0};
-  }
-  update() {
-    this.setState({val: this.state.val + 1});
-  }
-  render() {
-    return (
-      <InnerComponent
-        update={this.update}
-        {...this.state}
-        {...this.props}
-      />
-    );
-  }
-}
-
-const Button = (props) => <button onClick={props.update}>
-    {props.txt} - {props.val}
-  </button>;
-
-const Label = (props) => <label onContextMenu={props.update}>
-    {props.txt} - {props.val}
-  </label>;
-
-let ButtonMixed = Mixin(Button);
-let LabelMixed= Mixin(Label);
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {val: 0};
+    this.update = this.update.bind(this);
+  }
+  update() {
+    this.setState({ val: ReactDOM.findDOMNode(this.refs.inp).value});
+  }
   render() {
     return (
       <div>
-        <ButtonMixed txt="Button"/>
-        <LabelMixed txt="Label"/>
+        <NumInput
+          ref="inp"
+          type="range"
+          min={0}
+          max={255}
+          step={0.1}
+          label="Red"
+          val={+this.state.val}
+          update={this.update}
+        />
+        <label>{this.state.val}</label>
       </div>
     )
   }
 }
+
+class NumInput extends React.Component {
+  render() {
+    return (
+      <input
+            type={this.props.type}
+            min={this.props.min}
+            max={this.props.max}
+            step={this.props.step}
+            defaultValue={this.props.val}
+            onChange={this.props.update}
+      />
+    )
+  }
+}
+
+NumInput.propTypes = {
+  min:    React.PropTypes.number,
+  max:    React.PropTypes.number,
+  step:   React.PropTypes.number,
+  val:    React.PropTypes.number,
+  label:  React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type:   React.PropTypes.oneOf(['number', 'range'])
+};
+
+NumInput.defaultProps = {
+  min:    0,
+  max:    255,
+  step:   1,
+  val:    0,
+  label:  '',
+  type:   'range'
+};
+
 
 export default App;
