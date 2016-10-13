6bec43082a5f7c1386ece5666cf9b3a0072b1c36
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-13 15:46:15 +0300

    EHREACT-12 react-react-fundamentals-higher-order-components-replaces-mixins

--- App.js
+++ App.js
@@ -1,42 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-console.log(React);
-class App extends React.Component {
+
+let Mixin = (InnerComponent) => class extends React.Component {
   constructor() {
     super();
     this.update = this.update.bind(this);
-    this.state = {increasing: false};
+    this.state = {val: 0};
   }
-
   update() {
-    ReactDOM.render(
-      <App val={this.props.val + 1}/>,
-      document.getElementById('app')
-    )
+    this.setState({val: this.state.val + 1});
   }
   render() {
-    console.log(this.state.increasing);
     return (
-      <button onClick={this.update}>
-        {this.props.val}
-      </button>
-    )
-  }
-  componentWillReceiveProps(nextProps) {
-    this.setState({increasing: nextProps.val > this.props.val});
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log('prevProps', prevProps);
-    console.log('prevState', prevState);
+      <InnerComponent
+        update={this.update}
+        {...this.state}
+        {...this.props}
+      />
+    );
   }
 }
 
-App.defaultProps = {
-  val: 0
-};
+const Button = (props) => <button onClick={props.update}>
+    {props.txt} - {props.val}
+  </button>;
+
+const Label = (props) => <label onContextMenu={props.update}>
+    {props.txt} - {props.val}
+  </label>;
+
+let ButtonMixed = Mixin(Button);
+let LabelMixed= Mixin(Label);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ButtonMixed txt="Button"/>
+        <LabelMixed txt="Label"/>
+      </div>
+    )
+  }
+}
 
 export default App;
-
