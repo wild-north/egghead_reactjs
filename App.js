ca338de8fce912229c90254dd632b3802ef1090d
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-14 12:54:36 +0300

    EHREACT-15 build-a-jsx-live-compiler

--- App.js
+++ App.js
@@ -1,38 +1,64 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
+
+    this.timeout = 2000;
+
     this.state = {
-      input: '/* add your JSX here */',
+      showError: false,
+      defaultInput: '/* add your JSX here */',
+      init: true,
       output: '',
-      err: ''
-    }
+      err: '',
+      input: ''
+    };
+
+    this.update = this.update.bind(this);
+    this.updateError = this.updateError.bind(this);
   }
   update(e) {
     let code = e.target.value;
+
     try {
       this.setState({
-        output: babel.transform(code, {
-          stage: 0,
-          loose: 'all'
-        }).code
+        output: babel.transform(code, {stage: 0, loose: 'all'}).code,
+        err: '',
+        showError: false
       })
     } catch (err) {
-      this.setState({err: err.message})
+      if (this.timerId) {
+        clearInterval(this.timerId);
+      }
+
+      if (!this.state.showError) {
+        this.timerId = setTimeout(() => {
+          this.updateError(err);
+        }, this.timeout);
+      } else {
+        this.updateError(err);
+      }
     }
   }
+  updateError(err) {
+    this.setState({
+      err: err.message,
+      showError: true
+    })
+  }
+
   render() {
     return (
-      <div>
-        <header>{this.state.err}</header>
+      <div className={this.state.err ? 'error' : ''}>
+        <header>{this.state.err ? `Error: ${this.state.err}` : ''}</header>
         <div className="container">
-          <textarea name="" id="" cols="30" rows="10"
-                    onChange={this.update}
-                    defaultValue={this.state.input}
+          <textarea
+            onChange={this.update}
+            placeholder={this.state.defaultInput}
           ></textarea>
-          <pre></pre>
+          <pre>{this.state.err ? this.state.err : this.state.output}</pre>
         </div>
       </div>
     );
